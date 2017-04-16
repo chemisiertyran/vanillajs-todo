d92c58268d46cf275bed81ddad24c09a03725c29
chemisiertyran <christian.meyer68@gmail.com>
2017-04-16 23:44:37 +0200

    implement delete todo feature

--- js/todolist.view.js
+++ js/todolist.view.js
@@ -18,6 +18,7 @@ module.exports = {
 
     bindEvents: function() {
         this.buttonEl.addEventListener("click", this.addTodo.bind(this));
+        this.listEl.addEventListener("click", this.deleteTodo.bind(this));
     },
 
     render: function() {
@@ -28,12 +29,28 @@ module.exports = {
     renderTodo: function(todoText) {
         var todoEl = document.createElement("li");
         todoEl.innerHTML = todoText;
+        todoEl.appendChild(this.renderDeleteTodo());
         this.listEl.appendChild(todoEl);
+    },
+
+    renderDeleteTodo: function() {
+        var deleteTodo = document.createElement("button");
+        deleteTodo.className = "todolist-delete";
+        deleteTodo.innerHTML = "Delete";
+        return deleteTodo;
     },
 
     addTodo: function() {
         var todoText = this.inputEl.value;
         this.todoModel.addTodo(todoText);
         this.render();
+    },
+
+    deleteTodo: function(event) {
+        if (event.target.matches(".todolist-delete")) {
+            let todoText = event.target.parentElement.innerText;
+            this.todoModel.removeTodo(todoText);
+            this.render();
+        }
     }
 };
